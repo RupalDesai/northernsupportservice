e7e91bc4f8095a1687239da7976415db59e97d94
Rupal Desai <35616791+RupalDesai@users.noreply.github.com>
2024-08-31 17:41:15 -0700

    Add files via upload

--- functionality/javascript/contactus.js
+++ functionality/javascript/contactus.js
@@ -26,3 +26,49 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
   
+
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+    const referral = document.getElementById('referral').value;
+
+    const data = {
+        service_id: 'service_q2debbn',
+        template_id: 'template_3bk8v1k',
+        user_id: '8yddtZXCraQjrxFhe',
+        template_params: {
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'message': message,
+            'referral': referral
+        }
+    };
+
+    fetch('https://api.emailjs.com/api/v1.0/email/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(data => {
+        document.getElementById('responseMessage').innerText = 'Message sent successfully!';
+        document.getElementById('responseMessage').style.display = 'block';
+        document.getElementById('contactForm').reset();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('responseMessage').innerText = 'An error occurred while sending the message.';
+        document.getElementById('responseMessage').style.display = 'block';
+    });
+});
+
+window.addEventListener('load', function() {
+    document.querySelector('.containerRotate').classList.add('visible');
+});
+
