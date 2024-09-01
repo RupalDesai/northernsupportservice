55866cf5ce2f31f851b150fb460e7b376fe4cfd7
Rupal Desai <35616791+RupalDesai@users.noreply.github.com>
2024-08-31 17:27:43 -0700

    Add files via upload

---
+++ contactus.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+  
+    // Get the button that opens the modal
+    var btn = document.getElementById("openModalBtn");
+  
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+  
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+  
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+  
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+  });
+  
+
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+    const referral = document.getElementById('referral').value;
+
+    const data = {
+        service_id: 'service_q2debbn',
+        template_id: 'template_3bk8v1k',
+        user_id: '8yddtZXCraQjrxFhe',
+        template_params: {
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'message': message,
+            'referral': referral
+        }
+    };
+
+    fetch('https://api.emailjs.com/api/v1.0/email/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(data => {
+        document.getElementById('responseMessage').innerText = 'Message sent successfully!';
+        document.getElementById('responseMessage').style.display = 'block';
+        document.getElementById('contactForm').reset();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('responseMessage').innerText = 'An error occurred while sending the message.';
+        document.getElementById('responseMessage').style.display = 'block';
+    });
+});
+
+window.addEventListener('load', function() {
+    document.querySelector('.containerRotate').classList.add('visible');
+});
+
